47f8d1eb61952c3c1a4c547bc690c858292877eb
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-19 20:20:08 +0530

    Get the coffee data from the client to server

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // create a post api to send data from client to server 
+    app.post('/coffee',async(req,res)=>{
+        const newCoffee = req.body;
+        console.log(newCoffee);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
